65d2cad6bda8a6daaab4acf1c1fa3751a2bb4713
asadbek2001773 <117082956+asadbek2001773@users.noreply.github.com>
2022-10-31 17:27:14 +0500

    while son

---
+++ 13-dars.py
@@ -0,0 +1,5 @@
+son = 1
+while son < 100:
+    print(son, end="\n")
+    son = son + 1
+    print("salom")
--- uy ishi/uy iwi 7.py
+++ uy ishi/uy iwi 7.py
@@ -1,101 +1,101 @@
 
-python={ 
-    'integer':'butun son', 
-    'float':'onlik son', 
-    'boolean':'mantiqiy qiymat', 
-    'for':'biror amalni qayta-qayta bajarishtsikli', 
-    'if':'shartlarni tekshirish operatorlari', 
-    'elif':'aks holda agar', 
-    'else':'aks holda', 
-    'get':"davom ettirish", 
-    'sort':'tartiblash', 
-    'sqrt':'ildizdan chiqarish' 
-    } 
-for key, value in sorted(python.items()): 
-    print(f"{key.title()}-{value.title()}") 
+# python={ 
+#     'integer':'butun son', 
+#     'float':'onlik son', 
+#     'boolean':'mantiqiy qiymat', 
+#     'for':'biror amalni qayta-qayta bajarishtsikli', 
+#     'if':'shartlarni tekshirish operatorlari', 
+#     'elif':'aks holda agar', 
+#     'else':'aks holda', 
+#     'get':"davom ettirish", 
+#     'sort':'tartiblash', 
+#     'sqrt':'ildizdan chiqarish' 
+#     } 
+# for key, value in sorted(python.items()): 
+#     print(f"{key.title()}-{value.title()}") 
  
  
  
 
-print("\n>>>DAVLATLAR ") 
-davlatlar={ 
-    "O'zbekiston", 
-    "aqsh", 
-    "roosia", 
-    "tojikiston", 
-    "qirg'iziston", 
-    "qozog'izton", 
-    "malayziya", 
-    "singapur", 
-    "italia" 
-    } 
-for davlatlar in sorted (davlatlar): 
-    print(davlatlar.title()) 
+# print("\n>>>DAVLATLAR ") 
+# davlatlar={ 
+#     "O'zbekiston", 
+#     "aqsh", 
+#     "roosia", 
+#     "tojikiston", 
+#     "qirg'iziston", 
+#     "qozog'izton", 
+#     "malayziya", 
+#     "singapur", 
+#     "italia" 
+#     } 
+# for davlatlar in sorted (davlatlar): 
+#     print(davlatlar.title()) 
  
  
 
  
-poytahtlar={ 
-    "toshkent", 
-    "washington", 
-    "maskva", 
-    "dushanbe", 
-    "bishkek", 
-    "nur sulton", 
-    "kuala-lumpur", 
-    "singapur", 
-    "rim" 
-    } 
-for poytahtlar in sorted (poytahtlar): 
-    print(poytahtlar.title()) 
+# poytahtlar={ 
+#     "toshkent", 
+#     "washington", 
+#     "maskva", 
+#     "dushanbe", 
+#     "bishkek", 
+#     "nur sulton", 
+#     "kuala-lumpur", 
+#     "singapur", 
+#     "rim" 
+#     } 
+# for poytahtlar in sorted (poytahtlar): 
+#     print(poytahtlar.title()) 
  
  
-davlatlar={ 
-    "vatikan":"vatikan", 
-    "ukraina":"kiyif", 
-    "hindiston":"dehli", 
-    "O'zbekiston":"toshkent", 
-    "aqsh":"washington", 
-    "roosia":"maskva", 
-    "tojikiston":"dushanbe", 
-    "qirg'iziston":"bishkek", 
-    "qozog'izton":"nur sulton", 
-    "malayziya":"kuala-lumpur", 
-    "singapur":"singapur", 
-    "italia":"rim" 
-    } 
-county=input("Qaysi davlatlarni poytahtini bilishni istaysiz?\n>>> ").lower() 
-capital=davlatlar.get(county) 
-if capital==None: 
-    print("Kechirasiz, bizda bu haqda malumot yo'q") 
-else: 
-    print(f"{county.upper()}nning poytaxti {capital.title()}") 
+# davlatlar={ 
+#     "vatikan":"vatikan", 
+#     "ukraina":"kiyif", 
+#     "hindiston":"dehli", 
+#     "O'zbekiston":"toshkent", 
+#     "aqsh":"washington", 
+#     "roosia":"maskva", 
+#     "tojikiston":"dushanbe", 
+#     "qirg'iziston":"bishkek", 
+#     "qozog'izton":"nur sulton", 
+#     "malayziya":"kuala-lumpur", 
+#     "singapur":"singapur", 
+#     "italia":"rim" 
+#     } 
+# county=input("Qaysi davlatlarni poytahtini bilishni istaysiz?\n>>> ").lower() 
+# capital=davlatlar.get(county) 
+# if capital==None: 
+#     print("Kechirasiz, bizda bu haqda malumot yo'q") 
+# else: 
+#     print(f"{county.upper()}nning poytaxti {capital.title()}") 
  
  
 
-menu={ 
-      "sir":24, 
-      "manti":31, 
-      "qozon kabob":49, 
-      "tandir somsa":12, 
-      "osh":17, 
-      "lag'mon":32, 
-      "non":7, 
-      "choy":6, 
-      "shashlik":29, 
-      "somsa":5, 
-      "tabaka":11 
-      } 
-narh=0 
-print("3ta taom buyurtma bering!\n>>> ") 
-buyurtmalar=[] 
-for i in range(5): 
-  buyurtmalar.append(input(f"{i+1}-taom: ").lower()) 
+# menu={ 
+#       "sir":24, 
+#       "manti":31, 
+#       "qozon kabob":49, 
+#       "tandir somsa":12, 
+#       "osh":17, 
+#       "lag'mon":32, 
+#       "non":7, 
+#       "choy":6, 
+#       "shashlik":29, 
+#       "somsa":5, 
+#       "tabaka":11 
+#       } 
+# narh=0 
+# print("3ta taom buyurtma bering!\n>>> ") 
+# buyurtmalar=[] 
+# for i in range(5): 
+#   buyurtmalar.append(input(f"{i+1}-taom: ").lower()) 
  
-for buyurtma in buyurtmalar: 
-    if buyurtma in menu: 
-        narh=narh+menu[buyurtma] 
-        print(f"{buyurtma.title()} {menu[buyurtma]} so'm ") 
-    else: 
-        print(f"kechirasiz, bizda {buyurtma} yo'q.");print(f"jami: {narh}")
+# for buyurtma in buyurtmalar: 
+#     if buyurtma in menu: 
+#         narh=narh+menu[buyurtma] 
+#         print(f"{buyurtma.title()} {menu[buyurtma]} so'm ") 
+#     else: 
+#         print(f"kechirasiz, bizda {buyurtma} yo'q.");print(f"jami: {narh}")
 
